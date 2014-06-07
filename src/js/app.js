52e8fd098d2c893b12815d265908ca899060a13d
James York <james.york@onstar.com>
2014-06-07 14:06:15 -0400

    got a basic version using old school js with everything clobbering the global namespace.

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,13 @@
-var CharacterManager = function() {
+var Character = {
+  aspects: {}
+};
 
-  var Character = {
-    aspects: {}
-  };
-
-  function saveBasicInfo() {
-    Character.playerName = document.getElementById("player-name").value;
-    Character.characterName = document.getElementById("character-name").value;
-    Character.template = document.getElementById("template").value;
-    Character.musts = document.getElementById("musts").value;
-    Character.aspects.highConcept = document.getElementById("high-concept").value;
-    Character.aspects.trouble = document.getElementById("trouble").value;
-    console.log(Character);
-  }
-
-  return {
-    saveBasicInfo: saveBasicInfo
-  };
-
-}();
+function saveBasicInfo() {
+  Character.playerName = document.getElementById("player-name").value;
+  Character.characterName = document.getElementById("character-name").value;
+  Character.template = document.getElementById("template").value;
+  Character.musts = document.getElementById("musts").value;
+  Character.aspects.highConcept = document.getElementById("high-concept").value;
+  Character.aspects.trouble = document.getElementById("trouble").value;
+  console.log(Character);
+}
