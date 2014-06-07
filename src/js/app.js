168c8dfb9792216465e5aa6209b1ca4b50f44148
James York <james.york@onstar.com>
2014-06-07 14:04:21 -0400

    got a basic module pattern working

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+var CharacterManager = function() {
+
+  var Character = {
+    aspects: {}
+  };
+
+  function saveBasicInfo() {
+    Character.playerName = document.getElementById("player-name").value;
+    Character.characterName = document.getElementById("character-name").value;
+    Character.template = document.getElementById("template").value;
+    Character.musts = document.getElementById("musts").value;
+    Character.aspects.highConcept = document.getElementById("high-concept").value;
+    Character.aspects.trouble = document.getElementById("trouble").value;
+    console.log(Character);
+  }
+
+  return {
+    saveBasicInfo: saveBasicInfo
+  };
+
+}();
