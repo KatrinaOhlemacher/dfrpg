48ea70c45d8e6043886a4b473f3302dae50588d5
James York <james.york@onstar.com>
2014-06-29 17:13:49 -0400

    added aspects fields and wired them up

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
   var Character={
-    aspects: {}
+    aspects: {},
+    phases: []
   };
 
-  saveBasicInfo = function(){
+  $("#save-basic-info").click(function(){
     Character.playerName = $("#player-name").val();
     Character.characterName = $("#character-name").val();
     Character.template = $("#template").val();
@@ -12,10 +13,58 @@ $(document).ready(function(){
     Character.aspects.highConcept = $("#highConcept").val();
     Character.aspects.trouble = $("#trouble").val();
     console.log(Character);
-  };
-  saveAspects = function () {
-    Character.aspects.thing = $("#thing").val();
-    console.log(Character);
-  };
+  });
 
+  $("#save-aspects").click(function(){
+    var phase = {};
+    savePhaseOne();
+    savePhaseTwo();
+    savePhaseThree();
+    savePhaseFour();
+    savePhaseFive();
+
+    console.log(Character);
+  });
+
+  function savePhaseOne(){
+    var phaseOne = {}
+    phaseOne.story = $("#background-story").val();
+    Character.aspects.background = $("#background-aspect").val();
+    Character.phases.push(phaseOne);
+  }
+
+  function savePhaseTwo(){
+    var phaseTwo = {}
+    phaseTwo.story = $("#rising-conflict-story").val();
+    Character.aspects.risingConflict = $("#rising-conflict-aspect").val();
+    Character.phases.push(phaseTwo);
+  }
+
+  function savePhaseThree(){
+    var phaseThree={}
+    phaseThree.title = $("#first-adventure-title").val();
+    phaseThree.story  = $("#first-adventure").val();
+    phaseThree.guest = $("#first-adventure-guest").val();
+    Character.aspects.firstAdventure = $("#first-adventure-aspect").val();
+    Character.phases.push(phaseThree);
+  }
+
+  function savePhaseFour(){
+    var phaseFour={}
+    phaseFour.title = $("#guest-story-title").val();
+    phaseFour.story  = $("#guest-events").val();
+    phaseFour.guest = $("#story-owner").val();
+    Character.aspects.guestStar = $("#guest-aspect").val();
+    Character.phases.push(phaseFour);
+  }
+
+
+  function savePhaseFive(){
+    var phaseFive={}
+    phaseFive.title = $("#guest-story-title2").val();
+    phaseFive.story  = $("#guest-events2").val();
+    phaseFive.guest = $("#story-owner2").val();
+    Character.aspects.guestStarRedux = $("#guest-aspect2").val();
+    Character.phases.push(phaseFive);
+  }
 });
